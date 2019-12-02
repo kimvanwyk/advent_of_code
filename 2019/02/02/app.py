23bf6fa24abb37890fc887f8aaa98ce620f841ed
Kim van Wyk <kimv@sahomeloans.com>
2019-12-02 11:06:38 +0200

    Solving step 2

--- 2019/02/02/app.py
+++ 2019/02/02/app.py
@@ -6,12 +6,6 @@ import operator
 #           [2,4,4,5,99,0],
 #           [1,1,1,4,99,5,6,0,99]]
 
-with open('input.txt', 'r') as fh:
-    inputs = [[int(i) for i in fh.readline().strip().split(',')],]
-
-inputs[0][1] = 12
-inputs[0][2] = 2
-
 ops = {1: operator.add,
        2: operator.mul}
 
@@ -23,8 +17,13 @@ def process(inputs):
         inputs[inputs[n+3]] = ops[inputs[n]](inputs[inputs[n+1]], inputs[inputs[n+2]])
         n += 4
     return inputs
-    
-for i in inputs:
-    print(i)
-    print(process(i))
-    print()
+
+for noun in range(100):
+    for verb in range(100):
+        with open('input.txt', 'r') as fh:
+            inputs = [[int(i) for i in fh.readline().strip().split(',')],]
+        inputs[0][1] = noun
+        inputs[0][2] = verb
+        process(inputs[0])    
+        if inputs[0][0] == 19690720:
+            print (noun,verb,(100*noun) + verb)
