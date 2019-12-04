4939af0f7f870fb43753a0b847a6320fec505ab5
Kim van Wyk <kimv@sahomeloans.com>
2019-12-04 11:26:43 +0200

    Better solution for step 1

--- 2019/04/04/app.py
+++ 2019/04/04/app.py
@@ -7,11 +7,11 @@ stop = 789857
 possibilities = []
 n = start
 while n <= stop:
-    s = f"{n}x"
-    if (all([s[j] <= s[j + 1] for j in range(len(s) - 1)])) and (
-        any([((s[j] == s[j + 1]) and (s[j + 1] != s[j + 2])) for j in range(len(s) - 1)])
-    ):
-        possibilities.append(n)
+    s = f"{n}"
+    if all([s[j] <= s[j + 1] for j in range(len(s) - 1)]):
+        ret = ''.join([str(int(s[j+1]) - int(s[j])) for j in range(len(s) - 1)])
+        if '0' in ret:
+            possibilities.append(n)
     n += 1
 print(possibilities)
 print(len(possibilities))
