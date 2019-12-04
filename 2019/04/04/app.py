fd46e75685bced74f8a63e50bb6a9d751fe35e2b
Kim van Wyk <kimv@sahomeloans.com>
2019-12-04 11:43:29 +0200

    Solution to step 2

--- 2019/04/04/app.py
+++ 2019/04/04/app.py
@@ -1,16 +1,19 @@
+import re
+
 start = 240920
 stop = 789857
 
-# start = 100
-# stop = 123
+# start = 11000
+# stop = 11423
 
+pat = re.compile('00+')
 possibilities = []
 n = start
 while n <= stop:
     s = f"{n}"
     if all([s[j] <= s[j + 1] for j in range(len(s) - 1)]):
         ret = ''.join([str(int(s[j+1]) - int(s[j])) for j in range(len(s) - 1)])
-        if '0' in ret:
+        if '0' in pat.sub('', ret):
             possibilities.append(n)
     n += 1
 print(possibilities)
