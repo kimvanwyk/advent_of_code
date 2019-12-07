bebd7e229a26dace2d158c75cb523051ec6a5489
Kim van Wyk <kimv@sahomeloans.com>
2019-12-07 14:19:46 +0200

    Step 1 complete

--- 2019/05/05/app.py
+++ 2019/05/05/app.py
@@ -7,7 +7,7 @@ inputs = [1]
 instructions = []
 with open('input.txt', 'r') as fh:
     for l in fh:
-        instructions.append(l.strip().split(','))
+        instructions.extend(l.strip().split(','))
 
 ops = {1: operator.add,
        2: operator.mul}
@@ -20,19 +20,22 @@ lengths = {1: 3,
 
 def get_modes(op):
     s = str(op)
+    if len(s) < 2:
+        s = f'0{s}'
     code = int(s[-2:])
     modes = [int(c) for c in s[:-2][::-1]]
     if len(modes) < lengths[code]:
         modes.extend([0] * (lengths[code] - len(modes)))
-    if code in ops:
+    if code in ops or code in (3,):
         modes[-1] = 1
     return (code, modes)
 
 def get_params(instructions, params, modes):
     out = []
+    # print('in params: p:', params, 'm:', modes)
     for (p,m) in zip(params, modes):
-        print(p,m)
         if m == 0:
+            # print(instructions[int(p)])
             out.append(int(instructions[int(p)]))
         if m == 1:
             out.append(int(p))
@@ -42,36 +45,33 @@ def process(instructions, inputs=[]):
     n = 0
     out = []
     while True:
+        print(instructions[:40])
+        print('n:', n, 'instructions n->n+10:', instructions[n:n+10])
         (code,modes) = get_modes(instructions[n])
         if code == 99:
             break
         params = get_params(instructions, instructions[n+1:n+lengths[code]+1], modes)
-        print(code)
-        print(modes)
-        print(params)
+        print('c:', code, 'm:', modes, 'p:', params)
+        # print(code)
+        # print(modes)
+        # print(params)
         if code in ops:
+            print(ops[code].__name__, params[0], 'and', params[1], ', write to pos ', params[2])
             instructions[params[2]] = ops[code](params[0], params[1])
         if code == 3:
-            instructions[params[0]] = inputs.pop(0)
+            i = inputs.pop(0)
+            print(f'writing {i} to position {params[0]}')
+            instructions[params[0]] = i
         if code == 4:
-            out.append(instructions[params[0]])
+            i = params[0]
+            print(f'reading {i} from position {params[0]}')
+            out.append(i)
         n += lengths[code] + 1
-        
-    return (out, instructions)
+        print()
+        # input()
 
-# print(get_modes(1002))
+    return (out, instructions)
 
 (out, instructions) = process(instructions, inputs)
 print(instructions)
 print(out)
-
-
-# for noun in range(100):
-#     for verb in range(100):
-#         with open('input.txt', 'r') as fh:
-#             inputs = [[int(i) for i in fh.readline().strip().split(',')],]
-#         inputs[0][1] = noun
-#         inputs[0][2] = verb
-#         process(inputs[0])    
-#         if inputs[0][0] == 19690720:
-#             print (noun,verb,(100*noun) + verb)
