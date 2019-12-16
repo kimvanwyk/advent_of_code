18f5fcabd249c8836d16c0c98767c56a1e5d0bb1
Kim van Wyk <kimv@sahomeloans.com>
2019-12-16 19:20:01 +0200

    Correct line function

--- 2019/10/10/app.py
+++ 2019/10/10/app.py
@@ -14,21 +14,21 @@ for a in asteroids:
 
 # print(len(asteroids), len(asteroids[-1]))
 
-mx = len(asteroids[-1])
-my = len(asteroids)
+mx = len(asteroids[-1]) - 1
+my = len(asteroids) - 1
 edges = []
-for x in range(mx):
+for x in range(mx + 1):
     edges.append((x,0))
-for y in range(my):
+for y in range(my + 1):
     edges.append((0,y))
-for x in range(mx):
-    edges.append((x,my - 1))
-for y in range(my):
-    edges.append((mx - 1,y))
+for x in range(mx + 1):
+    edges.append((x,my))
+for y in range(my + 1):
+    edges.append((mx,y))
+edges = list(set(edges))
+edges.sort()
 # edges = [(3,0),(2,0),(1,0),(0,0),(0,1)]
 # edges = [(2,0),(1,0),(0,0),(0,1)]
-print('edges:', edges)
-print()
 
 (x,y) = (3,4)
 num_hits = 0
@@ -37,7 +37,10 @@ for (ex,ey) in edges:
         m = 1
         dx = 0
     else:
-        dx = 1
+        if ex < x:
+            dx = -1
+        else:
+            dx = 1
         if ey == y:
             m = 0
         else:
@@ -48,43 +51,43 @@ for (ex,ey) in edges:
 
     points = []
     (sx,sy) = (x,y)
-    if x > ex:
-        op = operator.sub
-    else:
-        op = operator.add
-    if m is not None:
-        while True:
-            sx = op(sx, dx)
-            sy = sy + m
-            if not ((sx >= 0) and (sx < mx) and (sy >= 0) and (sy < my)):
+    while True:
+        sx += dx
+        sy += m
+        if not ((sx >= 0) and (sx <= mx) and (sy >= 0) and (sy <= my)):
+            break
+        # print('op:', op, 'sx:', sx, 'sy:', sy, 'int:', (not sx % 1) and (not sy % 1))
+        if (not sx % 1) and (not sy % 1):
+            points.append((int(sx), int(sy)))
+    print('points:', points)
+    if points:
+        for (px, py) in points:
+            if asteroids[py][px] == '#':
+                num_hits += 1
+                print('hit at', (px, py))
                 break
-            # print('op:', op, 'sx:', sx, 'sy:', sy, 'int:', (not sx % 1) and (not sy % 1))
-            if (not sx % 1) and (not sy % 1):
-                points.append((int(sx), int(sy)))
-        print('points:', points)
-        if points:
-            for (px, py) in points:
-                if asteroids[py][px] == '#':
-                    num_hits += 1
-                    print('hit at', (px, py))
-                    break
-            res = []
-            for a in asteroids:
-                res.append(a[:])
-            for (px, py) in points:
-                if res[py][px] == '#':
-                    p = 'h'
-                else:
-                    p = 'X'
-                res[py][px] = p
-            if res[ey][ex] == '#':
-                p = 'H'
+        res = []
+        for a in asteroids:
+            res.append(a[:])
+        for (px, py) in points:
+            if res[py][px] == '#':
+                p = 'h'
             else:
-                p = 'E'
-            res[ey][ex] = p
-            res[y][x] = 'S'
+                p = 'X'
+            res[py][px] = p
+        if res[ey][ex] in ('#', 'h'):
+            p = 'H'
+        else:
+            p = 'E'
+        res[ey][ex] = p
+        res[y][x] = 'S'
+        if 1:
             for a in res:
                 print(' '.join(a))
 
         print()
+for a in asteroids:
+    print(' '.join(a))
 print(num_hits)
+print('edges:', edges)
+print()
