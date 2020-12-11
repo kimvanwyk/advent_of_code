da9cd188f1dabdcf8daa4680d6b712f7c06951c0
Kim van Wyk <kimv@sahomeloans.com>
2020-12-11 14:02:23 +0200

    Part 2 done

--- 2020/01/01.py
+++ 2020/01/01.py
@@ -11,11 +11,13 @@ if TEST:
 else:
     input_data = common.input_data
 
-# Part 1: split = 2
-split = 2
+# Part 1: split = 2; total = 2020
+# Part 1: split = 3; total = 2020
+split = 3
+total = 2020
 
 for t in itertools.combinations(input_data, split):
-    if sum(t) == 2020:
+    if sum(t) == total:
         print(t, math.prod(t))
         pyperclip.copy(math.prod(t))
         break
