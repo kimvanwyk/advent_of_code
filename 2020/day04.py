b843c5f292691a17af2591c1b29dcdd9aeafde76
Kim van Wyk <kimv@sahomeloans.com>
2020-12-12 20:30:20 +0200

    hgt and hcl

--- 2020/day04.py
+++ 2020/day04.py
@@ -30,8 +30,9 @@ def check_passport_part1(passport):
     )
 
 
-def check_passport_part2(passport):
+def check_passport_part2(passport, height_ranges={"cm": (150, 193), "in": (59, 76)}):
     try:
+        # year range tests
         for (k, low, high) in (
             ("byr", 1920, 2002),
             ("iyr", 2010, 2020),
@@ -40,8 +41,27 @@ def check_passport_part2(passport):
             if not low <= int(passport[k]) <= high:
                 return False
 
+        hgt = passport["hgt"]
+        unit = hgt[-2:]
+        if unit not in height_ranges:
+            return False
+        if not height_ranges[unit][0] <= int(hgt[:-2]) <= height_ranges[unit][1]:
+            return False
+
+        hcl = passport["hcl"]
+        if not all(
+            [
+                hcl[0] == "#",
+                len(hcl) == 7,
+                all([c in "0123456789abcdef" for c in hcl[1:]]),
+            ]
+        ):
+            return False
+
         return True
     except Exception as e:
+        if settings.settings.debug:
+            print(e)
         return False
 
 
