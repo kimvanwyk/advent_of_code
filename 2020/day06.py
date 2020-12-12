e37e4d7e323cbad6bc8746427e2da65d2f873b17
Kim van Wyk <kimv@sahomeloans.com>
2020-12-12 21:43:27 +0200

    Include group size

--- 2020/day06.py
+++ 2020/day06.py
@@ -6,20 +6,27 @@ import settings
 
 
 def process():
-    groups = [defaultdict(int)]
+    groups = [[defaultdict(int),]]
+    n = 0
     input_data = common.read_string_file()
     for l in input_data:
         if l:
+            n += 1
             for c in l:
-                groups[-1][c] += 1
+                groups[-1][0][c] += 1
         else:
-            groups.append(defaultdict(int))
+            groups[-1].append(n)
+            n = 0
+            groups.append(
+                [defaultdict(int),]
+            )
+    groups[-1].append(n)
     return groups
 
 
 def part_1():
     groups = process()
-    lens = [len(d.keys()) for d in groups]
+    lens = [len(d.keys()) for (d, num) in groups]
     if settings.settings.debug:
         pprint.pprint(groups)
         print(lens)
