252aa0cfe3e09abdb5cd6cce87f5ab4f53a5ae3d
Kim van Wyk <kimv@sahomeloans.com>
2020-12-13 21:25:22 +0200

    New day templating

---
+++ 2020/template.py
@@ -0,0 +1,15 @@
+import common
+import settings
+
+
+def process():
+    input_data = common.read_string_file()
+    return ""
+
+
+def part_1():
+    return process()
+
+
+def part_2():
+    return process()
