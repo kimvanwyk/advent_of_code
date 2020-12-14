569871a4227797fa3d268eafd596070c4c277fc0
Kim van Wyk <kimv@sahomeloans.com>
2020-12-14 09:37:09 +0200

    Always add 0 and final value

--- 2020/day10.py
+++ 2020/day10.py
@@ -7,14 +7,14 @@ import settings
 
 def process():
     adapters = [n for n in common.read_integer_file()]
+    adapters.append(0)
+    adapters.sort()
+    adapters.append(adapters[-1] + 3)
     return adapters
 
 
 def part_1():
     adapters = process()
-    adapters.append(0)
-    adapters.sort()
-    adapters.append(adapters[-1] + 3)
     debug(adapters)
     counter = Counter()
     for n in range(1, len(adapters)):
