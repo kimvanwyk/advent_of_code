43d50927185ab1cdc7505a5848b9c6e704f3edfc
Kim van Wyk <kimv@sahomeloans.com>
2020-12-15 20:24:41 +0200

    Use dict

--- 2020/day11.py
+++ 2020/day11.py
@@ -13,35 +13,32 @@ class Grid:
     grid = attr.ib(default=attr.Factory(list))
     current_idx = attr.ib(default=(1, 1))
     current_val = attr.ib(default=None)
-    adjacent = attr.ib(default=None)
     changed = attr.ib(default=None)
+    width = attr.ib(default=0)
+    height = attr.ib(default=0)
     empty_seat_threshold = attr.ib(default=4)
 
     def make_grid(self, input_data):
-        self.grid = []
-        for line in input_data:
-            self.grid.append(["."])
-            self.grid[-1].extend([c for c in line])
-            self.grid[-1].append(".")
-        self.grid.insert(0, ["."] * len(self.grid[-1]))
-        self.grid.append(["."] * len(self.grid[-1]))
-        self.set_adjacent()
+        self.grid = {}
+        for (row, line) in enumerate(input_data):
+            self.grid.update({((row, col), c) for (col, c) in enumerate(line)})
+        self.width = len(line)
+        self.height = row + 1
+        self.show_grid()
+
+    def loop_grid(self):
+        for row in range(self.height):
+            for col in range(self.width):
+                yield (row, col, self.grid[(row, col)])
 
     def show_grid(self):
-        debug([line[1:-1] for line in self.grid[1:-1]], pretty=True)
+        out = []
+        for row in range(self.height):
+            out.append("  ".join(self.grid[(row, col)] for col in range(self.width)))
+        debug("\n".join(out))
 
-    def set_current_idx(self, x, y):
-        self.current_idx = (x, y)
-        self.current_val = self.grid[x][y]
-        self.set_adjacent()
-
-    def update_val(self, val):
-        (x, y) = self.current_idx
-        self.grid[x][y] = val
-        self.current_val = val
-
-    def set_adjacent(self):
-        self.adjacent = defaultdict(int)
+    def get_adjacent(self, x, y):
+        adjacent = defaultdict(int)
         for (row, col) in (
             (-1, -1),
             (-1, 0),
@@ -52,31 +49,27 @@ class Grid:
             (1, 0),
             (1, 1),
         ):
-            (cx, cy) = self.current_idx
-            self.adjacent[self.grid[cx + row][cy + col]] += 1
+            adjacent[self.grid.get((x + row, y + col), "B")] += 1
+        return adjacent
 
     def count_types(self):
         types = defaultdict(int)
-        for row in range(1, len(self.grid) - 1):
-            for col in range(1, len(self.grid[0]) - 1):
-                types[self.grid[row][col]] += 1
+        for (row, col, val) in self.loop_grid():
+            types[self.grid[(row, col)]] += 1
         return types
 
     def apply_occupation_rules(self):
         self.changes = []
-        for row in range(1, len(self.grid) - 1):
-            for col in range(1, len(self.grid[0]) - 1):
-                self.set_current_idx(row, col)
-                if self.current_val == "L" and (self.adjacent.get("#", 0) == 0):
-                    self.changes.append((row, col, "#"))
-                elif self.current_val == "#" and (
-                    self.adjacent.get("#", 0) >= self.empty_seat_threshold
-                ):
-                    self.changes.append((row, col, "L"))
+        for (row, col, val) in self.loop_grid():
+            adj = self.get_adjacent(row, col)
+            if val == "L" and (adj.get("#", 0) == 0):
+                self.changes.append((row, col, "#"))
+            elif val == "#" and (adj.get("#", 0) >= self.empty_seat_threshold):
+                self.changes.append((row, col, "L"))
 
         self.changed = bool(self.changes)
         for (row, col, val) in self.changes:
-            self.grid[row][col] = val
+            self.grid[(row, col)] = val
 
     def apply_occupation_rules_until_stable(self):
         self.changed = True
