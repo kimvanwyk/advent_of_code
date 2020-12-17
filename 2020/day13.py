9f0a4174f9cef26dcd74e10633fc12667c418b23
Kim van Wyk <kimv@sahomeloans.com>
2020-12-17 10:37:51 +0200

    Process more than 1 bus time

--- 2020/day13.py
+++ 2020/day13.py
@@ -6,15 +6,19 @@ import settings
 def process():
     input_data = [l for l in common.read_string_file()]
     timestamp = int(input_data[0])
-    buses = [int(bus) if bus != "x" else None for bus in input_data[1].split(",")]
-    debug(buses)
-    return (timestamp, buses)
+    bus_times = []
+    for bus_time in [bt for bt in input_data[1:]]:
+        bus_times.append(
+            [int(bus) if bus != "x" else None for bus in bus_time.split(",")]
+        )
+    debug(bus_times)
+    return (timestamp, bus_times)
 
 
 def part_1():
-    (timestamp, buses) = process()
+    (timestamp, bus_times) = process()
     floor = timestamp - 1
-    delays = [(((floor // bus) + 1) * bus, bus) for bus in buses if bus]
+    delays = [(((floor // bus) + 1) * bus, bus) for bus in bus_times[0] if bus]
     delays.sort()
     result = (delays[0][0] - timestamp) * delays[0][1]
     debug(delays)
