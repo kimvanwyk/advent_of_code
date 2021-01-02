90eb1aeec9f11840b2b1a30468695c63cc53f40d
Kim van Wyk <kimv@sahomeloans.com>
2021-01-02 16:33:08 +0200

    Part 2 complete

--- 2020/day19.py
+++ 2020/day19.py
@@ -56,8 +56,7 @@ def match_message(rules, message, rule=0, index=0):
     return hits
 
 
-def part_1():
-    (rules, messages) = process()
+def find_match_count(rules, messages):
     match_count = 0
     for message in messages:
         matches = match_message(rules, message)
@@ -66,29 +65,13 @@ def part_1():
     return match_count
 
 
+def part_1():
+    (rules, messages) = process()
+    return find_match_count(rules, messages)
+
+
 def part_2():
     (rules, messages) = process(
         {"8: 42": "8: 42 | 42 8", "11: 42 31": "11: 42 31 | 42 11 31"}
     )
-
-    rules["r8"] = "({r42})+"
-    run = []
-    for n in range(1, LONGEST_LINE + 1):
-        run.append(f"(({{r42}}[{n}])({{r31}}[{n}]))")
-    rules["r11"] = f"({'|'.join(run)})"
-
-    pattern = rules["r0"]
-    while "{" in pattern:
-        pattern = pattern.format(**rules)
-    pattern = pattern.replace("[", "{").replace("]", "}")
-    # print(pattern)
-    pat = regex.compile(pattern)
-
-    match_count = 0
-    for msg in messages:
-        m = bool(pat.match(msg))
-        debug(f"{msg}: {m}")
-        if m:
-            match_count += 1
-
-    return match_count
+    return find_match_count(rules, messages)
