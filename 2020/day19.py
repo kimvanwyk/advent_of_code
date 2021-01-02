c8509413375fc45b1269f0b75340b8f7cf1dd7e4
Kim van Wyk <kimv@sahomeloans.com>
2021-01-02 16:29:12 +0200

    Part 1 working again

--- 2020/day19.py
+++ 2020/day19.py
@@ -15,38 +15,53 @@ def process(mapping={}):
             break
         line = mapping.get(line, line)
         (r, pattern) = line.split(":")
-        p = []
-        if "|" in pattern:
-            p.append("(")
+        p = [[]]
         for c in pattern.split(" "):
             if c.strip():
                 if c == "|":
-                    p.append("|")
-                elif c[0] in "0123456789":
-                    p.append(f"{{r{c}}}")
+                    p.append([])
+                elif c[0] in "123456789":
+                    p[-1].append(int(c))
                 else:
-                    p.append(c.strip('"'))
-        if "|" in pattern:
-            p.append(")")
-        rules[f"r{r}"] = "".join(p)
+                    p = c.strip('"')
+        rules[int(r)] = p if type(p) is str else tuple(p)
     messages = [line for line in input_data]
     return (rules, messages)
 
 
+# heavily influenced by https://github.com/mebeim/aoc/blob/master/2020/README.md#day-19---monster-messages
+def match_message(rules, message, rule=0, index=0):
+    if index >= len(message):
+        return []
+
+    debug(f"Rule Num: {rule}; index: {index}; rule: {rules[rule]}; message: {message}")
+    rule = rules[rule]
+    # if rule is a string and it matches, return next index as this match is done
+    if type(rule) is str:
+        if message[index] == rule:
+            return [index + 1]
+        return []
+
+    hits = []
+    for ruleset in rule:
+        ruleset_hits = [index]
+
+        for sub_rule in ruleset:
+            new_hits = []
+            for idx in ruleset_hits:
+                new_hits += match_message(rules, message, sub_rule, idx)
+            ruleset_hits = new_hits
+
+        hits += ruleset_hits
+    return hits
+
+
 def part_1():
     (rules, messages) = process()
-    pattern = f"^{rules['r0']}$"
-    while "{" in pattern:
-        pattern = pattern.format(**rules)
-    debug(pattern)
-    pat = regex.compile(pattern)
-
     match_count = 0
-    for msg in messages:
-        m = bool(pat.match(msg))
-        debug(f"{msg}: {m}")
-        if m:
-            match_count += 1
+    for message in messages:
+        matches = match_message(rules, message)
+        match_count += any(len(message) == m for m in matches)
 
     return match_count
 
