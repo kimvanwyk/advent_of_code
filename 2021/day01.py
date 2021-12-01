3e0275fde0852b4437a9ac5225aef764b546e419
kimv <kimv@sahomeloans.com>
2021-12-01 21:24:59 +0200

    Day 1 Part 2

--- 2021/day01.py
+++ 2021/day01.py
@@ -3,7 +3,7 @@ from common import debug
 import settings
 
 
-def process():
+def process_p1():
     inc = 0
     input_data = common.read_integer_file()
     first = next(input_data)
@@ -15,9 +15,34 @@ def process():
     return inc
 
 
+def process_p2():
+    inc = 0
+    input_data = common.read_integer_file()
+    windows = [[], [], []]
+    # prime first 3 windows
+    for n in range(3):
+        val = next(input_data)
+        while n >= 0:
+            windows[n].append(val)
+            n -= 1
+    for val in input_data:
+        windows.append([])
+        for n in (-3, -2, -1):
+            windows[n].append(val)
+    debug(windows)
+    sums = {0: sum(windows[0])}
+    for (n, w) in enumerate(windows[1:], 1):
+        if len(w) < 3:
+            break
+        if sums.setdefault(n, sum(w)) > sums[n - 1]:
+            inc += 1
+    debug(sums)
+    return inc
+
+
 def part_1():
-    return process()
+    return process_p1()
 
 
 def part_2():
-    return process()
+    return process_p2()
