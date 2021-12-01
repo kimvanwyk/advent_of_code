a47f8e78d0276b952b75a4621758c0df6566c8da
kimv <kimv@sahomeloans.com>
2021-12-01 20:56:59 +0200

    2020 errant uncommitted file

--- 2020/day20.py
+++ 2020/day20.py
@@ -62,19 +62,57 @@ def find_unique_edge_count(tiles):
     return edge_counts
 
 
-def find_layout(tiles):
+def layout_tiles(tiles):
     pairs = []
     edges = {}
     for (t1, t2) in combinations(tiles.keys(), 2):
         edges1 = edges.setdefault(t1, get_edges(tiles[t1]))
         edges2 = edges.setdefault(t2, get_edges(tiles[t2]))
-        for (s1, e1) in tiles[t1].items():
-            for (s2, e2) in tiles[t2].items():
+        for (s1, e1) in edges1:
+            for (s2, e2) in edges2:
                 if e1 == e2:
                     pairs.append(((t1, s1, False), (t2, s2, False)))
                 elif e1 == e2[::-1]:
                     pairs.append(((t1, s1, False), (t2, s2, True)))
-    return pairs
+
+    # map sides to rotations
+    rotations = {
+        ("l", "r"): 0,
+        ("l", "t"): 90,
+        ("l", "l"): 180,
+        ("l", "b"): 270,
+        ("t", "b"): 0,
+        ("t", "r"): 90,
+        ("t", "t"): 180,
+        ("t", "l"): 270,
+        ("r", "l"): 0,
+        ("r", "b"): 90,
+        ("r", "r"): 180,
+        ("r", "t"): 270,
+        ("b", "t"): 0,
+        ("b", "l"): 90,
+        ("b", "b"): 180,
+        ("b", "r"): 270,
+    }
+    # map t1 sides to position of next tile
+    positions = {"l": (-1, 0), "b": (0, -1), "r": (1, 0), "t": (0, 1)}
+
+    layout = {pairs[0][0][0]: (100, 100)}
+    while True:
+        skipped = False
+        for ((t1, s1, f1), (t2, s2, f2)) in pairs:
+            if t1 not in layout:
+                skipped = True
+            else:
+                (x1, y1) = layout[t1]
+                (x2, y2) = positions[s1]
+                layout[t2] = (x1 + x2, y1 + y2)
+                print(
+                    f"{t1} {s1} -> {s2} {t2} (flipped: {f2}). Rotation: {rotations[(s1, s2)]}; positions: {layout[t2]}"
+                )
+        if not skipped:
+            break
+    print(layout)
 
 
 def part_1():
@@ -85,5 +123,5 @@ def part_1():
 
 def part_2():
     tiles = process()
-    pairs = find_layout(tiles)
+    pairs = layout_tiles(tiles)
     return pairs
