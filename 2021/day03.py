f2f3e7af2f50a5681a4426fb3480529f12d61db8
kimv <kimv@sahomeloans.com>
2021-12-03 13:31:13 +0200

    Day 3 Part 2

--- 2021/day03.py
+++ 2021/day03.py
@@ -25,9 +25,33 @@ def process_p1():
     return int("".join(gamma), 2) * int("".join(epsilon), 2)
 
 
+def process_p2():
+    vals = {}
+    for (key, count_index, tiebreak) in (("oxygen", 0, "1"), ("co2", 1, "0")):
+        input_data = [l for l in common.read_string_file()]
+        debug(key)
+        for i in range(len(input_data[0])):
+            c = Counter()
+            for l in input_data:
+                c[l[i]] += 1
+            mc = c.most_common(2)
+            debug(mc)
+            if (len(mc) > 1) and (mc[0][1] == mc[1][1]):
+                keepbit = tiebreak
+            else:
+                keepbit = mc[count_index][0]
+            input_data = [l for l in input_data if l[i] == keepbit]
+            debug(f"{i=}  {c=}  {input_data=}  {keepbit=}")
+            if len(input_data) == 1:
+                vals[key] = int("".join(input_data[0]), 2)
+                break
+    debug(vals)
+    return vals["oxygen"] * vals["co2"]
+
+
 def part_1():
     return process_p1()
 
 
 def part_2():
-    return process()
+    return process_p2()
