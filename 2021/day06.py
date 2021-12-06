9d1e3ce718c6e071d19284a0120449704ec8d520
kimv <kimv@sahomeloans.com>
2021-12-06 10:21:56 +0200

    Using np.array

--- 2021/day06.py
+++ 2021/day06.py
@@ -2,34 +2,33 @@ import common
 from common import debug
 import settings
 
+import numpy as np
+
 
 FIRST_DEBUG_DAYS = 18
-TARGET_DAY = 80
 
 
-def process():
+def process(target_day):
     input_data = common.read_string_file()
-    fishes = [int(i) for i in next(input_data).split(",")]
+    fishes = np.array([int(i) for i in next(input_data).split(",")], dtype=np.int8)
     day = 0
+    debug(fishes)
     while True:
         day += 1
-        new_fish_count = 0
-        for i in range(len(fishes)):
-            fishes[i] -= 1
-            if fishes[i] < 0:
-                fishes[i] = 6
-                new_fish_count += 1
+        fishes -= 1
+        new_fish_count = (fishes == -1).sum()
+        fishes[fishes == -1] = 6
         if new_fish_count:
-            fishes.extend([8] * new_fish_count)
+            fishes = np.append(fishes, [8] * new_fish_count)
         if day <= FIRST_DEBUG_DAYS:
             debug(f"{day: 2}: {fishes}")
-        if day == TARGET_DAY:
+        if day == target_day:
             return len(fishes)
 
 
 def part_1():
-    return process()
+    return process(18 if settings.settings.test else 80)
 
 
 def part_2():
-    return process()
+    return process(256)
