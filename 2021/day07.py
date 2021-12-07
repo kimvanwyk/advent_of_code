196712e3e7c46ec541dae474970c5bb0f2b8d1ba
kimv <kimv@sahomeloans.com>
2021-12-07 09:52:00 +0200

    Require fuel cost func

--- 2021/day07.py
+++ 2021/day07.py
@@ -5,19 +5,29 @@ from common import debug
 import settings
 
 
-def process():
+# Thanks to the internet for reminding me that the sum of
+# [1,2,3...n,n+1,n+2]
+# for n is
+# (n * (n+1)) / 2
+
+
+def p1_fuel_cost(distance):
+    return distance
+
+
+def process(fuel_cost_func):
     input_data = common.read_string_file()
     crabs = [int(i) for i in next(input_data).split(",")]
     positions = defaultdict(int)
     for crab in crabs:
         for pos in range(min(crabs), max(crabs) + 1):
-            positions[pos] += abs(pos - crab)
+            positions[pos] += fuel_cost_func(abs(pos - crab))
     debug(positions)
     return min(positions.values())
 
 
 def part_1():
-    return process()
+    return process(p1_fuel_cost)
 
 
 def part_2():
