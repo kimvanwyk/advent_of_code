19e5f64437c5ae32bbf9ca8883a9ad2c04e79850
kimv <kimv@sahomeloans.com>
2021-12-10 23:53:50 +0200

    Towards d10 p1

---
+++ 2021/day10.py
@@ -0,0 +1,39 @@
+import common
+from common import debug
+import settings
+
+OPEN = "{[(<"
+CLOSE = "}])>"
+
+OPEN_TO_CLOSE = {"{": "}", "[": "]", "(": ")", "<": ">"}
+CLOSE_TO_OPEN = {v: k for (k, v) in OPEN_TO_CLOSE.items()}
+
+
+def process():
+    input_data = common.read_string_file()
+    mismatches = []
+    for line in input_data:
+        opens = []
+        for c in line:
+            if c in OPEN:
+                opens.append(c)
+            if c in CLOSE:
+                o = opens[-1] if opens else None
+                if OPEN_TO_CLOSE.get(o, None) != c:
+                    mismatches.append(c)
+                    debug(f"{line}- Mismatch on line")
+                    break
+                opens.pop()
+        else:
+            debug(f"{line} - No mismatch on line")
+    debug(mismatches)
+
+    return ""
+
+
+def part_1():
+    return process()
+
+
+def part_2():
+    return process()
