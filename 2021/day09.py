376d01cf815359a6d43829dfc54b38d2b68ca6b6
kimv <kimv@sahomeloans.com>
2021-12-10 21:16:42 +0200

    Incorrect first-pass at basin

--- 2021/day09.py
+++ 2021/day09.py
@@ -16,10 +16,11 @@ class Grid:
             self.width = x + 1
         self.height += 1
 
-    def show_grid(self):
+    def show_grid(self, spacer=""):
         if settings.settings.debug:
             for y in range(self.height):
-                print("".join(str(self.grid[(x, y)]) for x in range(self.width)))
+                print("".join(f"{self.grid[(x, y)]: 4}" for x in range(self.width)))
+            print()
 
     def process_low_points(self):
         self.risk = 0
@@ -34,6 +35,26 @@ class Grid:
                     self.risk += 1 + val
         return self.risk
 
+    def process_basins(self):
+        in_basin = False
+        basin_val = 0
+        for y in range(self.height):
+            for x in range(self.width):
+                val = self.grid[(x, y)]
+                if val == 9 and in_basin:
+                    in_basin = False
+                if 0 <= val < 9:
+                    if not in_basin:
+                        in_basin = True
+                        basin_val -= 1
+                        self.grid[(x, y)] = basin_val
+                if self.grid[(x, y)] < 0:
+                    for pos in ((x, y + 1), (x + 1, y)):
+                        neighbour = self.grid.get(pos, None)
+                        if neighbour not in (None, 9):
+                            self.grid[pos] = self.grid[(x, y)]
+        self.show_grid()
+
 
 def process():
     input_data = common.read_string_file()
@@ -41,12 +62,15 @@ def process():
     for line in input_data:
         grid.add_row(int(c) for c in line)
     grid.show_grid()
-    return grid.process_low_points()
+    return grid
 
 
 def part_1():
-    return process()
+    grid = process()
+    return grid.process_low_points()
 
 
 def part_2():
-    return process()
+    grid = process()
+    grid.process_basins()
+    return ""
