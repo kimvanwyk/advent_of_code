c6f7e3ac77f8fb93b172bdd4754491f970d8de57
kimv <kimv@sahomeloans.com>
2021-12-11 16:42:34 +0200

    Day 5 p 2

--- 2021/day10.py
+++ 2021/day10.py
@@ -7,12 +7,14 @@ CLOSE = "}])>"
 
 OPEN_TO_CLOSE = {"{": "}", "[": "]", "(": ")", "<": ">"}
 CLOSE_TO_OPEN = {v: k for (k, v) in OPEN_TO_CLOSE.items()}
-POINTS = {")": 3, "]": 57, "}": 1197, ">": 25137}
+CORRUPT_POINTS = {")": 3, "]": 57, "}": 1197, ">": 25137}
+INCOMPLETE_SCORE = {"(": 1, "[": 2, "{": 3, "<": 4}
 
 
 def process():
     input_data = common.read_string_file()
     mismatches = []
+    unused = []
     for line in input_data:
         opens = []
         for c in line:
@@ -27,13 +29,25 @@ def process():
                 opens.pop()
         else:
             debug(f"{line} - No mismatch on line")
-    debug(mismatches)
-    return sum(POINTS[m] for m in mismatches)
+            unused.append(opens)
+    return (mismatches, unused)
 
 
 def part_1():
-    return process()
+    (mismatches, unused) = process()
+    return sum(CORRUPT_POINTS[m] for m in mismatches)
 
 
 def part_2():
-    return process()
+    (mismatches, unused) = process()
+    scores = []
+    for u in unused:
+        scores.append(0)
+        u.reverse()
+        debug(u)
+        for c in u:
+            scores[-1] *= 5
+            scores[-1] += INCOMPLETE_SCORE[c]
+        debug(scores)
+    scores.sort()
+    return scores[len(scores) // 2]
