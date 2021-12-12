2c50b41d0979d3f0e5dead207cbdb6b4037924b8
kimv <kimv@sahomeloans.com>
2021-12-12 17:09:39 +0200

    Refinements to measure doubles

--- 2021/day12.py
+++ 2021/day12.py
@@ -1,3 +1,4 @@
+from collections import defaultdict
 import common
 from common import debug
 import settings
@@ -15,36 +16,54 @@ def process():
 
 
 class NodeProcessor:
-    def __init__(self, graph):
+    def __init__(self, graph, allow_double=False):
         self.graph = graph
         self.total_paths = 0
+        self.allow_double = allow_double
+        self.paths = []
 
-    def process(self, node, path):
+    def process(self, node, path, seen_double=False):
+        proceed = False
         for neighbour in self.graph.neighbors(node):
             # debug(f"{node=}  {neighbour=}")
             if neighbour == "end":
-                # debug(f"Good path: {path}")
+                debug(f"Good path: {path}")
+                if settings.settings.debug:
+                    self.paths.append(path)
                 self.total_paths += 1
                 continue
             elif neighbour == "start":
                 continue
-            elif (all(n in string.ascii_lowercase for n in neighbour)) and (
-                neighbour in path
-            ):
-                # hit a small cave again
-                # debug(f"Bad path (goes to {node}): {path}")
-                continue
+            elif all(n in string.ascii_lowercase for n in neighbour):
+                if neighbour in path:
+                    if not self.allow_double:
+                        continue
+                    else:
+                        if not seen_double:
+                            seen_double = True
+                            proceed = True
+                            continue
+                else:
+                    proceed = True
             else:
-                path.append(node)
-                self.process(neighbour, path[:])
+                proceed = True
+            if proceed:
+                if (not path) or path[-1] != node:
+                    path.append(node)
+                self.process(neighbour, path[:], seen_double)
 
 
 def part_1():
     graph = process()
-    np = NodeProcessor(graph)
+    np = NodeProcessor(graph, allow_double=False)
     np.process("start", [])
     return np.total_paths
 
 
 def part_2():
-    return process()
+    graph = process()
+    np = NodeProcessor(graph, allow_double=True)
+    np.process("start", [])
+    np.paths.sort()
+    debug(np.paths)
+    return np.total_paths
