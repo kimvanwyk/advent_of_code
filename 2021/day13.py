5cd519a89087d64921eaab828ad1684898610739
kimv <kimv@sahomeloans.com>
2021-12-13 21:13:50 +0200

    Change to class

--- 2021/day13.py
+++ 2021/day13.py
@@ -5,43 +5,44 @@ import settings
 import numpy as np
 
 
-def process():
-    d = {}
-    folds = []
-    input_data = common.read_string_file()
-    maxx = 0
-    maxy = 0
-    for line in input_data:
-        if not line.strip():
-            break
-        (x, y) = [int(l) for l in line.strip().split(",")]
-        d[(x, y)] = 1
-        if x >= maxx:
-            maxx = x + 1
-        if y >= maxy:
-            maxy = y + 1
+class Array:
+    def __init__(self):
+        d = {}
+        self.folds = []
+        input_data = common.read_string_file()
+        maxx = 0
+        maxy = 0
+        for line in input_data:
+            if not line.strip():
+                break
+            (x, y) = [int(l) for l in line.strip().split(",")]
+            d[(x, y)] = 1
+            if x >= maxx:
+                maxx = x + 1
+            if y >= maxy:
+                maxy = y + 1
 
-    array = np.full((maxy, maxx), False, dtype=bool)
-    for (x, y) in d.keys():
-        array[y][x] = True
+        self.array = np.full((maxy, maxx), False, dtype=bool)
+        for (x, y) in d.keys():
+            self.array[y][x] = True
 
-    for line in input_data:
-        if line:
-            vals = line[11:].split("=")
-            folds.append((vals[0], int(vals[1])))
-    return (array, folds)
+        for line in input_data:
+            if line:
+                vals = line[11:].split("=")
+                self.folds.append((vals[0], int(vals[1])))
 
+    def show(self):
+        if settings.settings.debug:
+            for row in self.array:
+                debug(" ".join("#" if col else "." for col in row))
 
-def show_array(array):
-    if settings.settings.debug:
-        for row in array:
-            debug(" ".join("#" if col else "." for col in row))
+    # def fold(self, fold):
 
 
 def part_1():
-    (array, folds) = process()
-    debug(folds)
-    show_array(array)
+    a = Array()
+    debug(a.folds)
+    a.show()
     return ""
 
 
