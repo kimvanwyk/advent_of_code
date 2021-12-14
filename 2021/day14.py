691a63e0c54deca24d827e2f5bf93d9fdf120b0e
kimv <kimv@sahomeloans.com>
2021-12-14 09:50:32 +0200

    D14 P1

--- 2021/day14.py
+++ 2021/day14.py
@@ -1,3 +1,5 @@
+from collections import Counter
+
 import common
 from common import debug
 import settings
@@ -14,7 +16,18 @@ def process():
 
 def part_1():
     (polymer, mappings) = process()
-    return ""
+    for step in range(10):
+        for i in range(len(polymer) - 1, 0, -1):
+            polymer.insert(i, mappings[f"{polymer[i-1]}{polymer[i]}"])
+        if step < 4:
+            debug(f"{step=}  {polymer=}")
+
+    count = Counter(polymer)
+    totals = count.most_common()
+    debug(f"Most common after {step+1} steps: {totals[0]}")
+    debug(f"Least common after {step+1} steps: {totals[-1]}")
+    debug(f"Difference: {totals[0][1] - totals[-1][1]}")
+    return totals[0][1] - totals[-1][1]
 
 
 def part_2():
