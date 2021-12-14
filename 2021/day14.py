9ff962944d8dc7da9f0fb8aa8a299bb9f179e65c
kimv <kimv@sahomeloans.com>
2021-12-14 09:54:18 +0200

    Use np array

--- 2021/day14.py
+++ 2021/day14.py
@@ -4,21 +4,22 @@ import common
 from common import debug
 import settings
 
+import numpy as np
+
 
 def process():
     input_data = common.read_string_file()
-    polymer = [c for c in next(input_data)]
+    polymer = np.array([c for c in next(input_data)])
     mappings = dict([(l.strip().split(" -> ")) for l in input_data if l])
     debug(polymer)
     debug(mappings)
     return (polymer, mappings)
 
 
-def part_1():
-    (polymer, mappings) = process()
-    for step in range(10):
+def build_polymer(polymer, mappings, steps):
+    for step in range(steps):
         for i in range(len(polymer) - 1, 0, -1):
-            polymer.insert(i, mappings[f"{polymer[i-1]}{polymer[i]}"])
+            polymer = np.insert(polymer, i, mappings[f"{polymer[i-1]}{polymer[i]}"])
         if step < 4:
             debug(f"{step=}  {polymer=}")
 
@@ -30,5 +31,11 @@ def part_1():
     return totals[0][1] - totals[-1][1]
 
 
+def part_1():
+    (polymer, mappings) = process()
+    return build_polymer(polymer, mappings, 10)
+
+
 def part_2():
-    return process()
+    (polymer, mappings) = process()
+    return build_polymer(polymer, mappings, 40)
