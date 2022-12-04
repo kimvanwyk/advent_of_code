b9213b8e71da624a8fd6b41941f94147e30b9d71
kimv <kimv@sahomeloans.com>
2022-12-04 13:36:04 +0200

    d3 p1

---
+++ 2022/day04.py
@@ -0,0 +1,25 @@
+import common
+from common import debug
+import settings
+
+
+def process():
+    return common.read_string_file()
+
+
+def part_1():
+    pairs = []
+    subsets = 0
+    for line in process():
+        pairs = [[int(i) for i in rng.split("-")] for rng in line.split(",")]
+        if (pairs[0][0] <= pairs[1][0] <= pairs[1][1] <= pairs[0][1]) or (
+            pairs[1][0] <= pairs[0][0] <= pairs[0][1] <= pairs[1][1]
+        ):
+            subsets += 1
+            debug(pairs)
+            debug(f"found match.")
+    return subsets
+
+
+def part_2():
+    return process()
