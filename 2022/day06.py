1e628a0385e6501299f88349281d943f61aa397a
kimv <kimv@sahomeloans.com>
2022-12-06 09:26:09 +0200

    d2 p2 with commonalisation

--- 2022/day06.py
+++ 2022/day06.py
@@ -5,25 +5,26 @@ import settings
 from collections import deque
 
 
-def process():
-    return common.read_string_file()
+def process(msglen):
+    indices = []
+    for line in common.read_string_file():
+        d = deque(maxlen=msglen)
+        d.extend(line[: msglen - 1])
+        debug(d)
+        for (n, c) in enumerate(line[msglen - 1 :], msglen):
+            d.append(c)
+            debug(f"{n=} {c=} {d=}")
+            if len(set(d)) == msglen:
+                indices.append(n)
+                break
+    return indices
 
 
 def part_1():
-    indices = []
-    for line in process():
-        d = deque(maxlen=4)
-        d.extend(line[:3])
-        debug(d)
-        for (n, c) in enumerate(line[3:], 4):
-            d.append(c)
-            debug(f"{n=} {c=} {d=}")
-            if len(set(d)) == 4:
-                indices.append(n)
-                break
-    print(indices)
+    print(process(4))
     return ""
 
 
 def part_2():
-    return process()
+    print(process(14))
+    return ""
