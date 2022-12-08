33afeb731131177ebe41b66f4da866ac4db11a3c
kimv <kimv@sahomeloans.com>
2022-12-08 09:06:35 +0200

    d8 p1

---
+++ 2022/day08.py
@@ -0,0 +1,38 @@
+import common
+from common import debug
+import settings
+
+import numpy as np
+
+
+def process():
+    input_data = common.read_string_file()
+    array = np.array([[int(c) for c in line] for line in input_data], dtype=np.uint8)
+    debug(array)
+    return array
+
+
+def part_1():
+    array = process()
+    visible = 2 * array.shape[0] + 2 * (array.shape[1] - 2)
+    for ((row, col), tree) in np.ndenumerate(array):
+        if 0 < row < array.shape[0] - 1 and 0 < col < array.shape[1] - 1:
+            for (name, ri, ci) in (
+                ("top", slice(None, row), col),
+                ("bottom", slice(row + 1, None), col),
+                ("left", row, slice(None, col)),
+                ("right", row, slice(col + 1, None)),
+            ):
+                debug(
+                    f"{name=}, {array[ri, ci]=}, {tree=}, {max(array[ri, ci]) < tree=}"
+                )
+                if max(array[ri, ci]) < tree:
+                    visible += 1
+                    # only care about any single visibility
+                    break
+            debug("")
+    return visible
+
+
+def part_2():
+    return process()
