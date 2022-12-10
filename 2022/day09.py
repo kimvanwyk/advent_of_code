79151fe88e2f6d09cd8769877b4fcc337c31fbf6
kimv <kimv@sahomeloans.com>
2022-12-10 16:03:43 +0200

    d9 p2

--- 2022/day09.py
+++ 2022/day09.py
@@ -15,6 +15,16 @@ DIRECTIONS = {
 }
 
 
+def draw(points, minx, miny, maxx, maxy):
+    items = list(points.items())
+    items.reverse()
+    points = dict([(v.ints(), str(k)) for (k, v) in items])
+    if settings.settings.debug:
+        for y in range(maxy, miny, -1):
+            print("".join([points.get((x, y), ".") for x in range(minx, maxx)]))
+        print()
+
+
 def process(num_points):
     visited = defaultdict(int)
     points = {}
@@ -28,20 +38,24 @@ def process(num_points):
                 for n in range(1, len(points)):
                     diff = points[n - 1] - points[n]
                     # if diff.r is 1.0, no action, head and tail are touching
-                    if diff.r == 2.0:
-                        # tail is a compass point away from head, move in suitable direction
-                        points[n] += DIRECTIONS[direction]
-                    elif diff.r > 2.0:
+                    # if diff.r == 2.0:
+                    #     # tail is a compass point away from head, move in suitable direction
+                    #     points[n] += DIRECTIONS[direction]
+                    if diff.r >= 2.0:
                         # tail is angled away from head, make a 1 step diagonal move
                         change = Point2D(
-                            *[int(math.copysign(1, p)) for p in diff.ints()]
+                            *[int(math.copysign(1 if p else 0, p)) for p in diff.ints()]
                         )
                         debug(f"{change=}")
                         points[n] += change
                     debug(
-                        f"{points[0].ints()=} {points[n].ints()=} {diff.ints()=} {diff.r=}"
+                        f"{points[n-1].ints()=} {points[n].ints()=} {diff.ints()=} {diff.r=}"
                     )
+
+                # if direction == "R" and distance == "17":
+                #     draw(points, 9985, 9985, 10021, 10021)
                 visited[points[len(points) - 1].ints()] += 1
+            # draw(points, 9985, 9985, 10021, 10021)
     return len(visited)
 
 
@@ -50,4 +64,4 @@ def part_1():
 
 
 def part_2():
-    return process()
+    return process(10)
