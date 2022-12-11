5ce89dcc5ad7d816680c08d7b10cac5558b8e769
kimv <kimv@sahomeloans.com>
2022-12-11 20:50:35 +0200

    Revert binary, use worry divisor

--- 2022/day11.py
+++ 2022/day11.py
@@ -5,71 +5,45 @@ import settings
 
 from attr import define, field
 
-# from gmpy2 import mpz
-
+import array
 import operator
-import os
 
 OPERATORS = {"*": operator.mul, "+": operator.add}
-ITEM_LENGTHS = {}
-
-
-def int_to_bytes(x: int) -> bytes:
-    return x.to_bytes((x.bit_length() + 7) // 8, "big")
-
-
-def int_from_bytes(xbytes: bytes) -> int:
-    return int.from_bytes(xbytes, "big")
 
 
 @define(slots=False)
 class Monkey:
-    key: int
+    items: None
     operator: type(operator.add)
     operand: int
     test: int
     throwees: []
-    reduce_worry: bool
+    worry_divisor: int
     inspections: int = field(init=False)
 
     def __attrs_post_init__(self):
         self.inspections = 0
 
     def process(self):
-        for n in range(1, ITEM_LENGTHS[self.key] + 1):
-            flocal = f"items_{self.key:02}_{n:05}.bin"
-            with open(flocal, "rb") as fh:
-                self.inspections += 1
-                item = int_from_bytes(bytes(fh.read()))
-                item = self.operator(
-                    item, self.operand if self.operand is not None else item
-                )
-                if self.reduce_worry:
-                    item = item // 3
-                other = self.throwees[item % self.test == 0]
-                ITEM_LENGTHS[other] += 1
-                with open(
-                    f"items_{other:02}_{ITEM_LENGTHS[other]:05}.bin", "wb"
-                ) as fout:
-                    fout.write(int_to_bytes(item))
-            os.remove(flocal)
-        ITEM_LENGTHS[self.key] = 0
+        while self.items:
+            self.inspections += 1
+            item = self.items.pop(0)
+            item = self.operator(
+                item, self.operand if self.operand is not None else item
+            )
+            item = item // self.worry_divisor
+            yield (item, self.throwees[item % self.test == 0])
 
 
-def process(reduce_worry):
+def process(worry_divisor):
     input_data = common.read_string_file()
     monkeys = {}
-    kargs = {"reduce_worry": reduce_worry}
+    kargs = {"worry_divisor": worry_divisor}
     for line in input_data:
         if "Monkey" in line:
-            kargs["key"] = int(line.split(" ")[-1][:-1])
+            key = int(line.split(" ")[-1][:-1])
         if "Starting" in line:
-            length = 0
-            for item in line.split(":")[-1].split(", "):
-                length += 1
-                with open(f"items_{kargs['key']:02}_{length:05}.bin", "wb") as fh:
-                    fh.write(int_to_bytes(int(item)))
-            ITEM_LENGTHS[kargs["key"]] = length
+            kargs["items"] = [int(item) for item in line.split(":")[-1].split(", ")]
         if "Operation" in line:
             (operator, operand) = line.split("old ")[-1].split(" ")
             kargs["operator"] = OPERATORS[operator]
@@ -83,7 +57,7 @@ def process(reduce_worry):
             kargs["throwees"] = {True: int(line.split("monkey ")[-1])}
         if "If false" in line:
             kargs["throwees"][False] = int(line.split("monkey ")[-1])
-            monkeys[kargs["key"]] = Monkey(**kargs)
+            monkeys[key] = Monkey(**kargs)
     return monkeys
 
 
@@ -91,14 +65,15 @@ def process_monkeys(monkeys, rounds):
     round = 0
     while round < rounds:
         for monkey in monkeys.values():
-            monkey.process()
+            for action in monkey.process():
+                monkeys[action[1]].items.append(action[0])
         # debug(f"{round=}  {monkeys=}")
         round += 1
     return monkeys
 
 
 def part_1():
-    monkeys = process(reduce_worry=True)
+    monkeys = process(worry_divisor=3)
     monkeys = process_monkeys(monkeys, 20)
     inspections = [m.inspections for m in monkeys.values()]
     inspections.sort(reverse=True)
