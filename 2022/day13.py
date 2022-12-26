5c8777fa76ad4a858935a0d507ea1556cbd664ec
kimv <kimv@sahomeloans.com>
2022-12-26 15:58:55 +0200

    Store, incorrect answer

--- 2022/day13.py
+++ 2022/day13.py
@@ -30,25 +30,50 @@ def compare_lists(left, right):
     debug(f"{left=} {right=}")
     result = NO_RESULT
     for (l, r) in zip_longest(left, right, fillvalue=None):
-        debug(f"{l=} {r=}")
+        # debug(f"{l=} {r=}")
         if not is_list(l) and not is_list(r):
             # 2 ints, compare
-            if l == r:
-                continue
-            if l < r:
+            if l is None:
+                # left side of list out of items
                 result = CORRECT
-            if r < l:
+            elif r is None:
+                # right side of list out of items
                 result = INCORRECT
+            elif l == r:
+                continue
+            elif l < r:
+                result = CORRECT
+            elif r < l:
+                result = INCORRECT
+            if result != NO_RESULT:
+                break
+
+        if not is_list(l):
+            l = [l]
+        if not is_list(r):
+            r = [r]
+
+        if len(r) > 0 and len(l) == 0:
+            result = CORRECT
+        elif len(l) > 0 and len(r) == 0:
+            result = INCORRECT
+        else:
+            result = compare_lists(l, r)
+
+        if result != NO_RESULT:
             break
     return result
 
 
 def part_1():
+    correct_sum = 0
     for (n, pair) in enumerate(process(), 1):
         result = compare_lists(*pair)
         debug(f"{n=} {result=}")
         debug("")
-    return ""
+        if result == CORRECT:
+            correct_sum += n
+    return correct_sum
 
 
 def part_2():
