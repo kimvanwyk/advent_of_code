887e02bb2eec59dad49ee736c32c1c54d57882f4
kimv <kimv@sahomeloans.com>
2022-12-26 21:26:38 +0200

    d13 p2

--- 2022/day13.py
+++ 2022/day13.py
@@ -4,6 +4,7 @@ import settings
 
 from collections import namedtuple
 from itertools import zip_longest
+from rich import print
 
 NO_RESULT = "No Result"
 CORRECT = "Correct"
@@ -22,34 +23,38 @@ def process():
                 yield pair
 
 
+def process_p2():
+    return [eval(l) for l in common.read_string_file() if l] + [[[2]], [[6]]]
+
+
 def is_list(obj):
     return hasattr(obj, "append")
 
 
 def compare_lists(left, right):
-    debug(f"{left=}")
-    debug(f"{right=}")
+    # debug(f"{left=}")
+    # debug(f"{right=}")
     result = NO_RESULT
     for (l, r) in zip_longest(left, right, fillvalue=None):
-        debug(f"{l=}")
-        debug(f"{r=}")
+        # debug(f"{l=}")
+        # debug(f"{r=}")
         if l is None:
             # left side of list out of items
-            debug("l out of items, CORRECT")
+            # debug("l out of items, CORRECT")
             result = CORRECT
         elif r is None:
             # right side of list out of items
-            debug("r out of items, INCORRECT")
+            # debug("r out of items, INCORRECT")
             result = INCORRECT
         elif not is_list(l) and not is_list(r):
             # 2 ints, compare
             if l == r:
                 continue
             elif l < r:
-                debug("l < r, CORRECT")
+                # debug("l < r, CORRECT")
                 result = CORRECT
             elif r < l:
-                debug("l > r, INCORRECT")
+                # debug("l > r, INCORRECT")
                 result = INCORRECT
         if result != NO_RESULT:
             return result
@@ -58,14 +63,14 @@ def compare_lists(left, right):
             l = [l]
         if not is_list(r):
             r = [r]
-        debug(f"{l=}")
-        debug(f"{r=}")
+        # debug(f"{l=}")
+        # debug(f"{r=}")
 
         if len(r) > 0 and len(l) == 0:
             result = CORRECT
-            debug("l empty, CORRECT")
+            # debug("l empty, CORRECT")
         elif len(l) > 0 and len(r) == 0:
-            debug("r empty, INCORRECT")
+            # debug("r empty, INCORRECT")
             result = INCORRECT
         else:
             result = compare_lists(l, r)
@@ -88,4 +93,16 @@ def part_1():
 
 
 def part_2():
-    return process()
+    inputs = process_p2()
+    debug(inputs)
+    output = [inputs[0]]
+    for ipt in inputs[1:]:
+        for (idx, opt) in enumerate(output):
+            # insert if input should come before current output
+            if compare_lists(ipt, opt) == CORRECT:
+                output.insert(idx, ipt)
+                break
+        else:
+            output.append(ipt)
+    debug(output)
+    return (output.index([[2]]) + 1) * (output.index([[6]]) + 1)
