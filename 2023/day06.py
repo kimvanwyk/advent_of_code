cd250c7820c67e9c701e9f500350d2f37a5c5dff
kimv <kimv@sahomeloans.com>
2023-12-10 18:15:16 +0200

    D6 p2

--- 2023/day06.py
+++ 2023/day06.py
@@ -13,25 +13,41 @@ def process():
     return list(zip(*vals))
 
 
+def process_p2():
+    vals = []
+    for l in common.read_string_file():
+        if l:
+            vals.append(
+                int("".join([v.strip() for v in l.split(":")[-1].strip().split()]))
+            )
+    return vals
+
+
+def count_ways(time, distance):
+    ways = 0
+    for t in range(1, time):
+        if ((time - t) * t) > distance:
+            ways += 1
+        else:
+            if ways:
+                break
+    return ways
+
+
 def part_1():
     races = process()
     debug(races)
     num_ways = []
     for time, distance in races:
-        ways = 0
-        for t in range(1, time):
-            if ((time - t) * t) > distance:
-                ways += 1
-            else:
-                if ways:
-                    num_ways.append(ways)
-                    break
-        debug(num_ways)
-        mult = 1
-        for ways in num_ways:
-            mult *= ways
+        num_ways.append(count_ways(time, distance))
+    debug(num_ways)
+    mult = 1
+    for ways in num_ways:
+        mult *= ways
     return mult
 
 
 def part_2():
-    return process()
+    race = process_p2()
+    debug(race)
+    return count_ways(*race)
