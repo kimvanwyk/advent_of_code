8528257ea640609969d0b9c3e7265867e25204e1
kimv <kimv@sahomeloans.com>
2023-12-11 09:16:04 +0200

    D7 p2

--- 2023/day07.py
+++ 2023/day07.py
@@ -51,9 +51,10 @@ class Hand:
     def __attrs_post_init__(self):
         self.cards = [c for c in self.hand]
 
-    @property
-    def typ(self):
-        s = Counter(self.cards)
+    def _typ(self, cards=None):
+        if cards is None:
+            cards = self.cards
+        s = Counter(cards)
         k = s.values()
         if len(s) == 1:
             return 7
@@ -69,28 +70,58 @@ class Hand:
             return 2
         return 1
 
+    @property
+    def typ(self):
+        return self._typ()
 
-def process():
+    @property
+    def typ_joker(self):
+        cards = self.cards[:]
+        s = Counter(cards)
+        mc = "A"
+        for c, freq in s.most_common():
+            if c != "J":
+                mc = c
+                break
+        for n, c in enumerate(cards):
+            if c == "J":
+                cards[n] = mc
+        debug((mc, self.cards, cards))
+        return self._typ(cards)
+
+
+def process(joker=False):
+    cards = []
+    if joker:
+        order = ORDER_P2
+    else:
+        order = ORDER_P1
     for l in common.read_string_file():
         spl = l.split(" ")
-        yield (spl[0], int(spl[1]))
-
-
-def part_1():
-    cards = []
-    for hand, bid in process():
-        h = Hand(hand, bid)
+        h = Hand(spl[0], int(spl[1]))
         cards.append(h)
-        debug((h.typ, *[ORDER_P1[c] for c in h.cards]))
+    # debug(
+    #     (
+    #         getattr(h, "typ" if not joker else "typ_joker"),
+    #         *[order[c] for c in h.cards],
+    #     )
+    # )
     cards.sort(
-        key=lambda h: (h.typ, [ORDER_P1[c] for c in h.cards]),
-    )
-    debug(cards)
+        key=lambda h: (
+            getattr(h, "typ" if not joker else "typ_joker"),
+            *[order[c] for c in h.cards],
+        )
+    ),
+    # debug(cards)
     mult = 0
     for n, c in enumerate(cards, 1):
         mult += n * c.bid
     return mult
 
 
+def part_1():
+    return process(joker=False)
+
+
 def part_2():
-    return process()
+    return process(joker=True)
