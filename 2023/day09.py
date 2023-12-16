ec683dd11fcf49a0f4d3ef5cb7346df4c8d5e827
kimv <kimv@sahomeloans.com>
2023-12-16 20:03:16 +0200

    d9 p2

--- 2023/day09.py
+++ 2023/day09.py
@@ -5,14 +5,10 @@ import settings
 from rich import print
 
 
-def process():
-    for l in common.read_string_file():
-        yield [int(c.strip()) for c in l.split()]
-
-
-def part_1():
+def process(location="end"):
     results = []
-    for history in process():
+    for l in common.read_string_file():
+        history = [int(c.strip()) for c in l.split()]
         debug(f"{history=}")
         rows = [history[:]]
         while True:
@@ -24,13 +20,20 @@ def part_1():
                 rows[-1].append(0)
                 break
         for n in range(len(rows) - 2, -1, -1):
-            rows[n].append(rows[n][-1] + rows[n + 1][-1])
+            if location == "end":
+                rows[n].append(rows[n][-1] + rows[n + 1][-1])
+            else:
+                rows[n].insert(0, rows[n][0] - rows[n + 1][0])
         for row in rows:
             debug(row)
-        results.append(rows[0][-1])
+        results.append(rows[0][-1 if location == "end" else 0])
     debug(results)
     return sum(results)
 
 
-def part_2():
+def part_1():
     return process()
+
+
+def part_2():
+    return process(location="start")
