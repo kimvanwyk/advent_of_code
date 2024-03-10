55555feaa3de2bac7b242b3d0aac29a9cba93db7
kimv <kimv@sahomeloans.com>
2024-03-10 14:01:41 +0200

    FUnctionise

--- 2023/day13.py
+++ 2023/day13.py
@@ -21,57 +21,60 @@ def process():
     yield d
 
 
+def find_line(d):
+    max_col = max([k[0] for k in d if k[1] == 1])
+    max_row = max([k[1] for k in d if k[0] == 1])
+    for line, max_direction, max_edge in (
+        ("col", max_col, max_row),
+        ("row", max_row, max_col),
+    ):
+        for direction in range(1, max_direction):
+            found = True
+            for edge in range(1, max_edge + 1):
+                c = 0
+                while True:
+                    if (direction - c) < 1 or (direction + c + 1) > max_direction:
+                        break
+                    if line == "col":
+                        left = d[(direction - c, edge)]
+                        right = d[(direction + 1 + c, edge)]
+                    else:
+                        left = d[(edge, direction - c)]
+                        right = d[(edge, direction + 1 + c)]
+                    debug(
+                        (
+                            direction,
+                            edge,
+                            c,
+                            (direction - c),
+                            (direction + c + 1),
+                            left,
+                            right,
+                        )
+                    )
+                    if left != right:
+                        found = False
+                        break
+                    c += 1
+                if found is False:
+                    break
+
+            if found is True:
+                return (line, direction)
+                break
+        if found is True:
+            break
+    return None
+
+
 def part_1():
     results = []
     for d in process():
-        # debug(d)
-        max_col = max([k[0] for k in d if k[1] == 1])
-        max_row = max([k[1] for k in d if k[0] == 1])
-        for line, max_direction, max_edge in (
-            ("col", max_col, max_row),
-            ("row", max_row, max_col),
-        ):
-            for direction in range(1, max_direction):
-                found = True
-                for edge in range(1, max_edge + 1):
-                    c = 0
-                    while True:
-                        if (direction - c) < 1 or (direction + c + 1) > max_direction:
-                            break
-                        if line == "col":
-                            left = d[(direction - c, edge)]
-                            right = d[(direction + 1 + c, edge)]
-                        else:
-                            left = d[(edge, direction - c)]
-                            right = d[(edge, direction + 1 + c)]
-                        debug(
-                            (
-                                direction,
-                                edge,
-                                c,
-                                (direction - c),
-                                (direction + c + 1),
-                                left,
-                                right,
-                            )
-                        )
-                        if left != right:
-                            found = False
-                            break
-                        c += 1
-                    if found is False:
-                        break
-
-                if found is True:
-                    results.append((line, direction))
-                    break
-            if found is True:
-                break
-
         debug(results)
         result = 0
-        for direction, index in results:
-            result += index * (1 if direction == "col" else 100)
+        results.append(find_line(d))
+    for direction, index in results:
+        result += index * (1 if direction == "col" else 100)
     return result
 
 
