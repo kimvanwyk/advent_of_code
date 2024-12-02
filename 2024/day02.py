c3c58b7f4f5d99a4e439668a1d40d42d6248bf87
kimv <kimv@sahomeloans.com>
2024-12-02 17:39:24 +0200

    Extract common

--- 2024/day02.py
+++ 2024/day02.py
@@ -7,30 +7,34 @@ from rich import print
 from itertools import pairwise
 
 
-def process(threshold):
-    total = 0
+def process():
     for l in common.read_string_file():
-        row = [int(c.strip()) for c in l.split(" ")]
-        order = None
-        fails = 0
-        for pair in pairwise(row):
-            if order is None:
-                if pair[0] == pair[1]:
-                    fails += 1
-                if pair[0] > pair[1]:
-                    order = (0, 1)
-                else:
-                    order = (1, 0)
-            if not 0 < pair[order[0]] - pair[order[1]] < 4:
+        yield [int(c.strip()) for c in l.split(" ")]
+
+
+def check_row(row, threshold):
+    order = None
+    fails = 0
+    for pair in pairwise(row):
+        if order is None:
+            if pair[0] == pair[1]:
                 fails += 1
-        if fails < threshold:
+            if pair[0] > pair[1]:
+                order = (0, 1)
+            else:
+                order = (1, 0)
+        if not 0 < pair[order[0]] - pair[order[1]] < 4:
+            fails += 1
+    return fails < threshold
+
+
+def part_1():
+    total = 0
+    for row in process():
+        if check_row(row, 1):
             total += 1
     return total
 
 
-def part_1():
-    return process(1)
-
-
 def part_2():
-    return process()
+    return process(1)
