ce4af24a517932685f81eedeb94cd4e4c4c42f60
kimv <kimv@sahomeloans.com>
2024-12-03 08:36:50 +0200

    funcify

--- 2024/day03.py
+++ 2024/day03.py
@@ -6,23 +6,27 @@ from rich import print
 
 import re
 
-PATTERN = re.compile("mul\(\d{1,3},\d{1,3}\)")
+PATTERN = re.compile("mul\(\d{1,3},\d{1,3}\)|do\(\)|don't\(\)")
 
 
 def process():
     return common.read_string_file()
 
 
-def part_1():
-    lines = process()
+def check_lines(lines):
     total = 0
     for line in lines:
         debug(line)
         for match in PATTERN.finditer(line):
-            (left, right) = (int(i) for i in match.group()[4:-1].split(","))
-            debug(left, right)
-            total += left * right
+            if "mul" in match.group():
+                (left, right) = (int(i) for i in match.group()[4:-1].split(","))
+                debug(left, right)
+                total += left * right
     return total
+
+
+def part_1():
+    return check_lines(process())
 
 
 def part_2():
