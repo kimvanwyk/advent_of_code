4d077c138986c286628f0a34432e96cddce43876
kimv <kimv@sahomeloans.com>
2024-12-03 08:32:34 +0200

    d3 p1

--- 2024/day03.py
+++ 2024/day03.py
@@ -4,14 +4,25 @@ import settings
 
 from rich import print
 
+import re
+
+PATTERN = re.compile("mul\(\d{1,3},\d{1,3}\)")
+
 
 def process():
-    input_data = common.read_string_file()
-    return ""
+    return common.read_string_file()
 
 
 def part_1():
-    return process()
+    lines = process()
+    total = 0
+    for line in lines:
+        debug(line)
+        for match in PATTERN.finditer(line):
+            (left, right) = (int(i) for i in match.group()[4:-1].split(","))
+            debug(left, right)
+            total += left * right
+    return total
 
 
 def part_2():
