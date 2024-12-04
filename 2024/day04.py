0564a3543716c659555639febb8f00badf0a5d0c
kimv <kimv@sahomeloans.com>
2024-12-04 08:30:33 +0200

    Specify start to identift

--- 2024/day04.py
+++ 2024/day04.py
@@ -5,22 +5,22 @@ import settings
 from rich import print
 
 
-def process():
+def process(start):
     d = {}
-    xes = []
+    starts = []
     for col, l in enumerate(common.read_string_file()):
         for row, c in enumerate(l):
             d[(row, col)] = c
-            if c == "X":
-                xes.append((row, col))
-    return (d, xes)
+            if c == start:
+                starts.append((row, col))
+    return (d, starts)
 
 
 def part_1():
-    (d, xes) = process()
-    debug(xes)
+    (d, starts) = process("X")
+    debug(starts)
     total = 0
-    for x in xes:
+    for start in starts:
         for direction in (
             (0, -1),
             (1, -1),
@@ -31,7 +31,7 @@ def part_1():
             (-1, 0),
             (-1, -1),
         ):
-            p = (x[0], x[1])
+            p = (start[0], start[1])
             success = True
             for letter in "MAS":
                 p = (p[0] + direction[0], p[1] + direction[1])
