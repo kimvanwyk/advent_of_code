0d2ae3a47780535ddeb332c0106f838188ba163d
kimv <kimv@sahomeloans.com>
2024-12-04 08:26:53 +0200

    d4 p1

--- 2024/day04.py
+++ 2024/day04.py
@@ -6,12 +6,42 @@ from rich import print
 
 
 def process():
-    input_data = common.read_string_file()
-    return ""
+    d = {}
+    xes = []
+    for col, l in enumerate(common.read_string_file()):
+        for row, c in enumerate(l):
+            d[(row, col)] = c
+            if c == "X":
+                xes.append((row, col))
+    return (d, xes)
 
 
 def part_1():
-    return process()
+    (d, xes) = process()
+    debug(xes)
+    total = 0
+    for x in xes:
+        for direction in (
+            (0, -1),
+            (1, -1),
+            (1, 0),
+            (1, 1),
+            (0, 1),
+            (-1, 1),
+            (-1, 0),
+            (-1, -1),
+        ):
+            p = (x[0], x[1])
+            success = True
+            for letter in "MAS":
+                p = (p[0] + direction[0], p[1] + direction[1])
+                # debug(p, letter)
+                if d.get(p, None) != letter:
+                    success = False
+                    break
+            if success:
+                total += 1
+    return total
 
 
 def part_2():
