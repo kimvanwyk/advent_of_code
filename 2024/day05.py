5ac958ed021c05de8c51264b33d478e24ba13024
kimv <kimv@sahomeloans.com>
2024-12-05 08:40:17 +0200

    d5 p1

--- 2024/day05.py
+++ 2024/day05.py
@@ -2,16 +2,38 @@ import common
 from common import debug
 import settings
 
+from collections import defaultdict
+
 from rich import print
 
 
 def process():
-    input_data = common.read_string_file()
-    return ""
+    ordering = defaultdict(list)
+    pages = []
+    for l in common.read_string_file():
+        if "|" in l:
+            (k, v) = l.split("|")
+            ordering[int(k)].append(int(v))
+        elif "," in l:
+            pages.append([int(c) for c in l.split(",")])
+    return (ordering, pages)
 
 
 def part_1():
-    return process()
+    (ordering, pages) = process()
+    debug(ordering)
+    total = 0
+    for page in pages:
+        seen = []
+        for c in page:
+            if any(p in seen for p in ordering[c]):
+                debug(f"fail: {page}")
+                break
+            seen.append(c)
+        else:
+            debug(f"success: {page}")
+            total += page[len(page) // 2]
+    return total
 
 
 def part_2():
