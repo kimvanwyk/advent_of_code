3db32ab6c756be8a7dafd1111b92e235412e8f06
kimv <kimv@sahomeloans.com>
2024-12-08 17:41:35 +0200

    d5 p2

--- 2024/day05.py
+++ 2024/day05.py
@@ -19,22 +19,50 @@ def process():
     return (ordering, pages)
 
 
+def check_ordering(page, ordering):
+    seen = []
+    for c in page:
+        if any(p in seen for p in ordering[c]):
+            return False
+            break
+        seen.append(c)
+    else:
+        return True
+
+
 def part_1():
     (ordering, pages) = process()
     debug(ordering)
     total = 0
     for page in pages:
-        seen = []
-        for c in page:
-            if any(p in seen for p in ordering[c]):
-                debug(f"fail: {page}")
-                break
-            seen.append(c)
-        else:
+        if check_ordering(page, ordering):
             debug(f"success: {page}")
             total += page[len(page) // 2]
     return total
 
 
 def part_2():
-    return process()
+    (ordering, pages) = process()
+    debug(ordering)
+    total = 0
+    for page in pages:
+        if check_ordering(page, ordering):
+            continue
+        debug(page)
+        i = 0
+        while True:
+            swapped = False
+            if page[i] in ordering:
+                for j in range(i):
+                    if page[j] in ordering[page[i]]:
+                        (page[j], page[i]) = (page[i], page[j])
+                        swapped = True
+                        i = 0
+                        break
+            if swapped is False:
+                i += 1
+            if i == len(page):
+                debug(page)
+                total += page[len(page) // 2]
+                break
+    return total
