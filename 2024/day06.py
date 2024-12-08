2b681c28f7a9989312c662da9a738446859baba8
kimv <kimv@sahomeloans.com>
2024-12-08 20:06:27 +0200

    Brute force d6 p2

--- 2024/day06.py
+++ 2024/day06.py
@@ -5,6 +5,7 @@ import settings
 from rich import print
 
 from collections import defaultdict
+from copy import copy
 
 DIRECTION = {"<": Point(-1, 0), "^": Point(0, -1), ">": Point(1, 0), "v": Point(0, 1)}
 TURN = {"^": ">", ">": "v", "v": "<", "<": "^"}
@@ -17,11 +18,11 @@ def process():
             d[Point(x, y)] = c
             if c in "<>^v":
                 start = Point(x, y)
-    return (d, start)
+    return (d, start, ((x + 1) * (y + 1)))
 
 
 def part_1():
-    (d, start) = process()
+    (d, start, size) = process()
     debug(start)
     direction = d[start]
     debug(direction)
@@ -44,4 +45,30 @@ def part_1():
 
 
 def part_2():
-    return process()
+    (orig_d, start, size) = process()
+    start_direction = orig_d[start]
+    debug(size)
+    # Replace start position so later re-treads treat it as empty
+    orig_d[start] = "."
+    options = 0
+    for k in orig_d:
+        steps = 1
+        if k == start:
+            continue
+        d = copy(orig_d)
+        d[k] = "#"
+        p = start
+        direction = start_direction
+        while True:
+            new_p = p + DIRECTION[direction]
+            if new_p not in d:
+                break
+            elif d[new_p] == "#":
+                direction = TURN[direction]
+            elif d[new_p] == ".":
+                p = new_p
+                steps += 1
+            if steps > (size * 3):
+                options += 1
+                break
+    return options
