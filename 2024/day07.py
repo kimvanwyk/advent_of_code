b221694bf84f82e8418ad7369c0e382ed17bbb21
kimv <kimv@sahomeloans.com>
2024-12-21 19:32:51 +0200

    d7 p1, using a far more efficient permutation library

--- 2024/day07.py
+++ 2024/day07.py
@@ -2,16 +2,44 @@ import common
 from common import debug
 import settings
 
+import more_itertools
 from rich import print
+
+from collections import defaultdict
+import itertools
+import operator
+
+OPS = {"+": operator.add, "*": operator.mul}
 
 
 def process():
-    input_data = common.read_string_file()
-    return ""
+    for l in common.read_string_file():
+        (k, v) = l.split(":")
+        yield (int(k), [int(c) for c in v.strip().split(" ")])
 
 
 def part_1():
-    return process()
+    total = 0
+    for test_val, vals in process():
+        debug((test_val, vals))
+        l = len(vals) - 1
+        ops = "*" * l + "+" * l
+        debug(f"{ops=}")
+        success = False
+        for operator_set in more_itertools.distinct_permutations(ops, l):
+            debug(operator_set)
+            val = vals[0]
+            for i, op in enumerate(operator_set, 1):
+                # debug(test_val, val, op, vals[i])
+                val = OPS[op](val, vals[i])
+                if val > test_val:
+                    break
+            if val == test_val:
+                success = True
+                break
+        if success:
+            total += test_val
+    return total
 
 
 def part_2():
