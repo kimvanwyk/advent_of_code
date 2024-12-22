3de2ec8f0cada4bbe4b92c2b89bf71c1f730dcd1
kimv <kimv@sahomeloans.com>
2024-12-22 21:14:20 +0200

    Centralise processing

--- 2024/day09.py
+++ 2024/day09.py
@@ -6,26 +6,38 @@ from rich import print
 
 
 def process():
-    vals = []
-    for l in common.read_string_file():
+    files = {}
+    frees = {}
+    for line in common.read_string_file():
         i = 0
-        for n, c in enumerate(l, 1):
+        j = 0
+        for n, c in enumerate(line, 1):
+            l = int(c)
             if n % 2:
                 # odd number, file slot
-                if int(c):
-                    vals.extend([i] * int(c))
+                if l:
+                    files[j] = {"length": l, "id": i}
             else:
-                if int(c):
-                    vals.extend(["."] * int(c))
+                if l:
+                    frees[j] = {"length": l}
                 i += 1
-    return vals
+            j += l
+    return (files, frees, j)
 
 
 def part_1():
-    vals = process()
+    (files, frees, max_idx) = process()
+    j = 0
+    vals = []
+    while j < max_idx:
+        if j in files:
+            vals.extend([files[j]["id"]] * files[j]["length"])
+        elif j in frees:
+            vals.extend(["."] * frees[j]["length"])
+        j += 1
     debug(f"{vals=}")
     while True:
-        if not "." in vals:
+        if "." not in vals:
             break
         i = vals.index(".")
         v = vals.pop(-1)
