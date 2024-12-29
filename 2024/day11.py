d5c25e1234a16b0c5485f318190e3a080ba0dddf
kimv <kimv@sahomeloans.com>
2024-12-29 21:39:31 +0200

    d11 p1

--- 2024/day11.py
+++ 2024/day11.py
@@ -6,12 +6,34 @@ from rich import print
 
 
 def process():
-    input_data = common.read_string_file()
-    return ""
+    for l in common.read_string_file():
+        return l.split(" ")
 
 
 def part_1():
-    return process()
+    stones = process()
+    debug(stones)
+
+    steps = 0
+    while steps < 25:
+        k = 0
+        while k < len(stones):
+            v = stones[k]
+            if v == "0":
+                stones[k] = "1"
+            elif not len(v) % 2:
+                sp = int(len(v) / 2)
+                left = v[:sp]
+                right = v[sp::]
+                stones[k : k + 1] = (str(int(left)), str(int(right)))
+                k += 1
+            else:
+                stones[k] = str(int(v) * 2024)
+            k += 1
+        if steps < 6:
+            debug(stones)
+        steps += 1
+    return len(stones)
 
 
 def part_2():
