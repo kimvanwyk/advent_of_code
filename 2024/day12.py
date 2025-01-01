7073513c18d96f171badcfea2ed652f80d3a3dc7
kimv <kimv@sahomeloans.com>
2025-01-01 20:43:29 +0200

    Pre region combining

--- 2024/day12.py
+++ 2024/day12.py
@@ -23,6 +23,8 @@ def part_1():
     (nodes, min_p, max_p) = process()
     debug((nodes, min_p, max_p))
     regions = defaultdict(list)
+    region_frequency = defaultdict(int)
+    region_map = {}
     for point, val in nodes.items():
         in_region = False
         perims = 0
@@ -32,10 +34,12 @@ def part_1():
                 perims += 1
             else:
                 if nodes[dp] == val:
-                    in_region = True
+                    if not in_region:
+                        in_region = True
+                        region = region_map[dp]
                 else:
                     # not in region, increase both sides perim val
-                    regions[nodes[dp]][-1]["perims"] += 1
+                    regions[region_map[dp]]["perims"] += 1
                     perims += 1
         # check for right and bottom map edges
         for dx, dy in ((1, 0), (0, 1)):
@@ -43,12 +47,13 @@ def part_1():
             if not dp.in_bounds(min_p, max_p):
                 perims += 1
         if not in_region:
-            regions[val].append({"area": 0, "perims": 0})
-        regions[val][-1]["area"] += 1
-        regions[val][-1]["perims"] += perims
-        # debug((point, val, regions[val][-1]))
-        if "X" in regions:
-            debug(regions["X"])
+            region_frequency[val] += 1
+            region = f"{val}{region_frequency[val]:02}"
+            regions[region] = {"area": 0, "perims": 0}
+        region_map[point] = region
+        regions[region]["area"] += 1
+        regions[region]["perims"] += perims
+        # debug((point, val, region, regions[region]))
     debug(regions)
     return
 
