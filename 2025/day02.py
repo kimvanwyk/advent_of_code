9d9318c790a2a7c19d829347933f20e948d65ced
kimv <kimv@sahomeloans.com>
2025-12-02 12:47:25 +0200

    D2 P2

--- 2025/day02.py
+++ 2025/day02.py
@@ -4,6 +4,8 @@ import settings
 
 from rich import print
 
+import itertools
+
 
 def process():
     with open(settings.settings.input_file, "r") as fh:
@@ -24,4 +26,17 @@ def part_1():
 
 
 def part_2():
-    return process()
+    total = 0
+    for start, end in process():
+        for id_num in range(int(start), int(end) + 1):
+            invalid = False
+            for batch_size in range(1, (len(str(id_num)) // 2) + 1):
+                batches = [b for b in itertools.batched(str(id_num), n=batch_size)]
+                if len(set(batches)) == 1:
+                    debug(batches)
+                    invalid = True
+                    total += id_num
+                if invalid:
+                    break
+
+    return total
