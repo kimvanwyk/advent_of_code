e40749a2cd0e29188b8834c72af95d20bc76506b
kimv <kimv@sahomeloans.com>
2025-12-06 17:32:04 +0200

    Split processor

--- 2025/day06.py
+++ 2025/day06.py
@@ -4,6 +4,7 @@ import settings
 
 from rich import print
 
+from collections import defaultdict
 import functools
 import operator
 import os
@@ -12,7 +13,7 @@ import sqlite3
 
 
 class Processor:
-    def __init__(self):
+    def process_part_1(self):
         try:
             os.remove("day06.db")
         except Exception:
@@ -22,6 +23,8 @@ class Processor:
         self.cur.execute("CREATE TABLE vals (col, value)")
         self.cur.execute("CREATE TABLE operators (col, operator)")
 
+        self.cols = defaultdict(list)
+        self.operators = {}
         for l in common.read_string_file():
             for n, m in enumerate(re.finditer("\\S+", l), 0):
                 if l[0] in ("+", "*"):
@@ -32,21 +35,33 @@ class Processor:
                     self.cur.execute(f"INSERT INTO vals VALUES ({n},'{m.group(0)}');")
         self.num_cols = n + 1
 
+    def process_part_2(self):
+        for l in common.read_string_file():
+            for i, c in enumerate(l, 0):
+                if c in ("+", "*"):
+                    self.operators[n] = operator.add if c == "+" else operator.mul
+                else:
+                    self.cols[i].append(c)
+            debug(self.operators)
+            debug(self.cols)
 
-def part_1():
-    processor = Processor()
-    total = 0
-    print(processor.num_cols)
-    for n in range(processor.num_cols):
-        op = (
+    def get_operator(self, col_num: int):
+        return (
             operator.add
-            if processor.cur.execute(
-                f"SELECT operator FROM operators WHERE col = {n}"
+            if self.cur.execute(
+                f"SELECT operator FROM operators WHERE col = {col_num}"
             ).fetchone()[0]
             == "+"
             else operator.mul
         )
-        debug(n, op)
+
+
+def part_1():
+    processor = Processor()
+    processor.process_part_1()
+    total = 0
+    for n in range(processor.num_cols):
+        op = processor.get_operator(n)
         result = functools.reduce(
             op,
             (
@@ -56,10 +71,30 @@ def part_1():
                 ).fetchall()
             ),
         )
-        debug(result)
         total += result
     return total
 
 
 def part_2():
-    return process()
+    processor = Processor()
+    total = 0
+    print(processor.num_cols)
+    for n in range(processor.num_cols):
+        op = processor.get_operator(n)
+        debug(n, op)
+        max_len = processor.cur.execute(
+            f"SELECT MAX(LENGTH(value)) FROM vals WHERE col = {n}"
+        ).fetchone()[0]
+        debug(max_len)
+        # result = functools.reduce(
+        #     op,
+        #     (
+        #         int(v[0])
+        #         for v in processor.cur.execute(
+        #             f"SELECT value FROM vals WHERE col = {n}"
+        #         ).fetchall()
+        #     ),
+        # )
+        # debug(result)
+        # total += result
+    return total
