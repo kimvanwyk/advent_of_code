54d81d7944a5ef643020a8931dc1bfd48d50e3e9
kimv <kimv@sahomeloans.com>
2025-12-07 18:15:53 +0200

    Depth first recursion, doesn't work

--- 2025/day07.py
+++ 2025/day07.py
@@ -9,7 +9,12 @@ class Processor:
     def __init__(self):
         self.init_beam_idx = 0
         self.splitter_rows = {}
-        for n, l in enumerate(common.read_string_file()):
+        self.num_universes = 0
+        n = -1
+        for l in common.read_string_file():
+            if not l:
+                break
+            n += 1
             splitters = {}
             for idx, c in enumerate(l):
                 if c == "S":
@@ -17,7 +22,10 @@ class Processor:
                 elif c == "^":
                     splitters[idx] = 1
             self.splitter_rows[n] = splitters
+        self.max_row = n
         debug([sr.keys() for sr in self.splitter_rows.values()])
+        debug(self.max_row)
+        debug(self.splitter_rows)
 
     def process(self):
         for l in common.read_string_file():
@@ -26,6 +34,19 @@ class Processor:
                 if c == "^":
                     splitters[idx] = 1
             yield splitters
+
+    def recurse_path(self, row_idx, beam_idx):
+        debug(row_idx, beam_idx, self.num_universes, self.max_row)
+        if row_idx >= self.max_row:
+            self.num_universes += 1
+            return
+        if beam_idx not in self.splitter_rows[row_idx + 1]:
+            # No spliiter on next row
+            self.recurse_path(row_idx + 1, beam_idx)
+        else:
+            # take both left and right path
+            for direction in (-1, 1):
+                self.recurse_path(row_idx, beam_idx + direction)
 
 
 def part_1():
@@ -52,3 +73,5 @@ def part_1():
 def part_2():
     processor = Processor()
     debug(processor.init_beam_idx)
+    processor.recurse_path(0, processor.init_beam_idx)
+    return processor.num_universes
