b8b5d08a231699d267e62eb52e1431b6763ddf97
kimv <kimv@sahomeloans.com>
2025-12-07 19:32:29 +0200

    Corrected depth first, still not working

--- 2025/day07.py
+++ 2025/day07.py
@@ -46,7 +46,7 @@ class Processor:
         else:
             # take both left and right path
             for direction in (-1, 1):
-                self.recurse_path(row_idx, beam_idx + direction)
+                self.recurse_path(row_idx + 1, beam_idx + direction)
 
 
 def part_1():
@@ -75,3 +75,12 @@ def part_2():
     debug(processor.init_beam_idx)
     processor.recurse_path(0, processor.init_beam_idx)
     return processor.num_universes
+    # beam_idxs = [processor.init_beam_idx]
+    # for row_idx in range(processor.max_row):
+    #     debug(row_idx, processor.splitter_rows[row_idx])
+    #     next_beam_idxs = []
+    #     for beam_idx in beam_idxs:
+    #         if beam_idx not in processor.splitter_rows[row_idx + 1]:
+    #             next_beam_idxs.append(beam_idx)
+    #         else:
+    #             next_beam_idxs.append(beam_idx)
