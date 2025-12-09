823c8df61377c72f0c334ed79cfaf85e305b6b15
kimv <kimv@sahomeloans.com>
2025-12-09 11:44:10 +0200

    class-ify

--- 2025/day08.py
+++ 2025/day08.py
@@ -8,33 +8,40 @@ from itertools import combinations
 import math
 
 
-def process():
-    points = {}
-    for n, l in enumerate(common.read_string_file()):
-        points[n] = tuple([int(c) for c in l.split(",")])
-    return points
+class Processor:
+    def __init__(self):
+        self.points = {}
+        for n, l in enumerate(common.read_string_file()):
+            self.points[n] = tuple([int(c) for c in l.split(",")])
+        debug(self.points)
 
+    def set_distances(self):
+        self.distances = []
+        for combination in combinations(self.points.keys(), r=2):
+            self.distances.append(
+                (
+                    self.get_distance(*combination),
+                    (self.points[combination[0]], self.points[combination[1]]),
+                    *combination,
+                )
+            )
+        self.distances.sort()
 
-def get_distance(p1, p2):
-    return math.sqrt(sum([math.pow((b - a), 2) for (a, b) in zip(p1, p2)]))
+    def get_distance(self, point_idx1, point_idx2):
+        return math.sqrt(
+            sum(
+                [
+                    math.pow((b - a), 2)
+                    for (a, b) in zip(self.points[point_idx1], self.points[point_idx2])
+                ]
+            )
+        )
 
 
 def part_1():
-    points = process()
-    debug(points)
-    distances = []
-    for combination in combinations(points.keys(), r=2):
-        (p1, p2) = (points[combination[0]], points[combination[1]])
-        distances.append(
-            (
-                get_distance(points[combination[0]], points[combination[1]]),
-                p1,
-                p2,
-                *combination,
-            )
-        )
-    distances.sort()
-    debug(distances[:5])
+    processor = Processor()
+    processor.set_distances()
+    debug(processor.distances[:5])
 
 
 def part_2():
