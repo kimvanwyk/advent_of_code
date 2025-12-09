37f2c67ca5416e8b14a8b278dbb75c869b307a71
kimv <kimv@sahomeloans.com>
2025-12-09 07:31:52 +0200

    Adding missing support files

---
+++ 2025/common.py
@@ -0,0 +1,66 @@
+from attrs import define
+from rich import print
+
+import settings
+
+from itertools import product
+
+
+def read_integer_file(fn=None):
+    return (int(line) for line in read_string_file(fn))
+
+
+def read_string_file(fn=None):
+    if fn is None:
+        fn = settings.settings.input_file
+    with open(fn, "r") as fh:
+        for line in fh:
+            yield line.strip()
+
+
+def debug(*m):
+    if settings.settings.debug:
+        print(*m)
+
+
+def idempotent(object):
+    return object
+
+
+@define(order=True)
+class Point:
+    x: int
+    y: int
+
+    def __add__(self, other):
+        return Point(self.x + other.x, self.y + other.y)
+
+    def __sub__(self, other):
+        return Point(self.x - other.x, self.y - other.y)
+
+    def __hash__(self):
+        return hash((self.x, self.y))
+
+    def in_bounds(self, min_point, max_point):
+        return (min_point.x <= self.x <= max_point.x) and (
+            min_point.y <= self.y <= max_point.y
+        )
+
+    def get_neighbour_points(self):
+        return [
+            self + Point(ix, iy)
+            for ix, iy in product(range(-1, 2), range(-1, 2))
+            if self + Point(ix, iy) != self
+        ]
+
+
+if __name__ == "__main__":
+    a = Point(1, 1)
+    print(a.get_neighbour_points())
+    # b = Point(6, 6)
+    # l = [(a - b), (b - a), (b - a)]
+    # l.sort()
+    # print(l)
+    # print(a.in_bounds(Point(1, 1), Point(10, 10)))
+    # c = Point(0, -1)
+    # print(c.in_bounds(Point(1, 1), Point(10, 10)))
---
+++ 2025/main.py
@@ -0,0 +1,42 @@
+import importlib
+
+import pyperclip
+
+import settings
+
+settings.settings.test = False
+
+
+def run_day_part(day, part, test_part=None):
+    settings.settings.set_day_and_part(day, part, test_part)
+    day_mod = importlib.import_module(f"day{day:02}")
+    return getattr(day_mod, f"part_{part}")()
+
+
+if __name__ == "__main__":
+    import argparse
+
+    parser = argparse.ArgumentParser("Executor for 2024 Advent of Code challenges")
+    parser.add_argument(
+        "day", type=int, choices=range(1, 26), help="The day to execute"
+    )
+    parser.add_argument("part", type=int, choices=(1, 2), help="The part to execute")
+    parser.add_argument(
+        "data", choices=("t", "p"), help="Whether to run with test (t) or prod (p) data"
+    )
+    parser.add_argument("-d", action="store_true", help="Whether to include debug")
+    parser.add_argument(
+        "-p",
+        choices=(1, 2, 3, 4, 5),
+        default=None,
+        type=int,
+        help="If test data is in parts, which part to use. Will override part setting",
+    )
+    args = parser.parse_args()
+
+    settings.settings.test = args.data == "t"
+    settings.settings.debug = args.d
+
+    result = run_day_part(args.day, args.part, args.p)
+    print(result)
+    pyperclip.copy(result)
